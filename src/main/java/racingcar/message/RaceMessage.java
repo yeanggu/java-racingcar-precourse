bb1ee6c777aea6480e0f84200d65bf7ee52d3b2a
yeeun <dpdms9621@naver.com>
2023-05-12 18:14:27 +0900

    [feat] Race 각종 메시지 저장

---
+++ src/main/java/racingcar/message/RaceMessage.java
@@ -0,0 +1,15 @@
+package racingcar.message;
+
+public class RaceMessage {
+
+    private RaceMessage() {}
+
+    public static String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static String INPUT_RACE_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static String RACE_RESULT_MESSAGE = "실행 결과";
+    public static String RESULT_SIGN = " : ";
+    public static String RACE_SIGN = "-";
+
+    public static String RACE_WINNER_RESULT = "최종 우승자 : ";
+
+}
