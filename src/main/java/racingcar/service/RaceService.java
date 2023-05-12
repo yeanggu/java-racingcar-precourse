fdad4fb839e9798f34068fba6620f891ce790bf1
yeeun <dpdms9621@naver.com>
2023-05-12 19:23:48 +0900

    [feat] 자동차 객체를 생성하는 기능

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,11 +1,17 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.Car;
 import racingcar.view.RaceView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RaceService {
 
     private RaceView raceView;
+    private ArrayList<Car> cars = new ArrayList<>();
 
     public RaceService() {
         this.raceView = new RaceView();
@@ -16,6 +22,15 @@ public class RaceService {
         raceView.inputNameMessage();
         String input = Console.readLine();
         // TODO : 예외 처리
+        newCars(input);
+    }
+
+    // 자동차 객체를 생성한다.
+    public void newCars(String input) {
+        List<String> carNames = Arrays.asList(input.split(","));
+        for (String carName : carNames) { // ,로 구분된 차 이름 List
+            cars.add(new Car(carName));
+        }
     }
 
     // 경주를 진행할 횟수를 입력받는다.
@@ -25,4 +40,6 @@ public class RaceService {
         // TODO : 예외 처리
         raceView.lineBreak();
     }
+
+
 }
