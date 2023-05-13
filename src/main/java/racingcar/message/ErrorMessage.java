8d701b2b53b6c12e32968a95d0393a59141ead2e
yeeun <dpdms9621@naver.com>
2023-05-13 17:09:57 +0900

    [feat] 에러 메시지 추가

--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -7,5 +7,6 @@ public class ErrorMessage {
     public static String COUNT_NUMBER_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 한다.";
     public static String CAR_NAME_SIZE_ERROR_MESSAGE = "[ERROR] 자동차 이름은 5자 이하여야 한다.";
     public static String CAR_NAME_EMPTY_ERROR_MESSAGE = "[ERROR] 자동차 이름에는 공백이 없어야 한다.";
+    public static String EMPTY_ERROR_MESSAGE = "[ERROR] 입력에는 빈 값을 입력할 수 없다.";
 
 }
